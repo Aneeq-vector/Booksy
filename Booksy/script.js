d21eeb992a3464f0295ada1421cd0ebb909bbc09
Ahmed Aneeq <ahmedaneeq.official@gmail.com>
2025-02-10 11:58:43 +0530

    Update
    10/02/2025

--- Booksy/script.js
+++ Booksy/script.js
@@ -7,3 +7,39 @@ addpopupbtn.addEventListener("click",function(){
     popupoverlay.style.display="block"
     popupbox.style.display="block"
 })
+
+// select cancel button
+var cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click", function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+// select container, add-book button, book-title, bookk-author-input, book-description
+var container = document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitleinput = document.getElementById("book-title-input")
+var bookauthorinput = document.getElementById("book-author-input")
+var bookdescriptioninput = document.getElementById("book-description-input")
+
+addbook.addEventListener("click", function(event){
+    event.preventDefault()
+    var div = document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML = `<h2>${booktitleinput.value}</h2>
+            <h5>${bookauthorinput.value}</h5>
+            <p>${bookdescriptioninput.value}</p>
+            <button id="deletebtn" onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    booktitleinput.value = "";
+    bookauthorinput.value = "";
+    bookdescriptioninput.value = "";
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+function deletebook(event)
+{
+    event.target.parentElement.remove()
+}
